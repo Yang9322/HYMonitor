b90d0d41f72fa2520349ca1cd029f1cc8007f19f
Yang9322 <576421791@qq.com>
2017-02-27 21:10:52 +0800

    Initial Commit

---
+++ HYMonitor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYMonitor
+//
+//  Created by He yang on 2017/2/27.
+//  Copyright © 2017年 He yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYMonitor/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HYMonitor
+//
+//  Created by He yang on 2017/2/27.
+//  Copyright © 2017年 He yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
